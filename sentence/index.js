4edd5802065f587536c239d1bbeea141e36bb144
ashtime99 <ashtime99@gmail.com>
2021-05-19 10:59:00 +0800

    Site updated: 2021-05-19 10:58:51

---
+++ sentence/index.js
@@ -0,0 +1,13 @@
+"use strict";
+$(document).ready(function() {
+	$("#fullpage").fullpage({
+		loopBottom: !0,
+		verticalCentered: !1
+	})
+});
+
+// $(document).ready(function() {
+// 	$('#fullpage').fullpage({
+// 		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90']
+// 	});
+// });
