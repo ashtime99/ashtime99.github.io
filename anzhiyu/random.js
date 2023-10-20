bd73e138dffb59427d700a1e99cb86afaae942ad
ashtime99 <2356530007@qq.com>
2023-10-20 15:55:02 +0800

    Site updated: 2023-10-20 15:55:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/18/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/20/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
